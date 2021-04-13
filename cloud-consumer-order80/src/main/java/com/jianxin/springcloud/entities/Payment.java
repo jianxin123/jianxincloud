914d7fa711a3a1f63be85f59606e395498b38d07
jianxin <615845421@qq.com>
2021-04-14 07:24:50 +0800

    消费者模块

---
+++ cloud-consumer-order80/src/main/java/com/jianxin/springcloud/entities/Payment.java
@@ -0,0 +1,17 @@
+package com.jianxin.springcloud.entities;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Payment implements Serializable {
+    private Long id;
+    //订单序列号
+    private String serial;
+}
--- cloud-provider-payment-8001/src/main/java/com/jianxin/springcloud/controller/PaymentController.java
+++ cloud-provider-payment-8001/src/main/java/com/jianxin/springcloud/controller/PaymentController.java
@@ -4,10 +4,7 @@ import com.jianxin.springcloud.entities.CommonResult;
 import com.jianxin.springcloud.entities.Payment;
 import com.jianxin.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
@@ -20,7 +17,7 @@ public class PaymentController {
 
     //对数据库是插入操作 用post
     @PostMapping(value = "/payment/create")
-    public CommonResult create(Payment payment){
+    public CommonResult create(@RequestBody Payment payment){
         int result = paymentService.create(payment);
         log.info("*******插入结果：" + result);
 
@@ -34,11 +31,11 @@ public class PaymentController {
 
     @GetMapping(value = "/payment/get/{id}")
     public CommonResult getPaymentById(@PathVariable("id") Long id){
-//        return new CommonResult(200,"3ewrwere");
         Payment payment = paymentService.getPaymentById(id);
         log.info("*******查询结果："+ payment);
 
         if(payment != null){
+
             return new CommonResult(200,"查询成功",payment);
         }else{
             return new CommonResult(444,"查询ID" +id+ "失败",null);
